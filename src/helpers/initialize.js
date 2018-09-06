abc030bd8e2cc9ec14c0eebbc4e4839f4380c207
3846masa <3846masahiro+git@gmail.com>
2018-09-07 08:26:42 +0900

    Add update-notifier

--- src/helpers/initialize.js
+++ src/helpers/initialize.js
@@ -1,10 +1,18 @@
 import fs from 'fs-extra';
 import libpath from 'path';
+import updateNotifier from 'update-notifier';
 import { addPath } from 'app-module-path';
 
 import { MEMI_MODULES_FOLDER } from '../const';
 
+const pkg = require('../../package.json');
+
 export async function initialize() {
+  updateNotifier({
+    pkg,
+    updateCheckInterval: 1000 * 60 * 60 * 24,
+  }).notify();
+
   if (!(await fs.pathExists(MEMI_MODULES_FOLDER))) {
     await fs.ensureDir(MEMI_MODULES_FOLDER);
   }
